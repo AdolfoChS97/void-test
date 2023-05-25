a8eda544fb433cafd146f057bdb2731c59726235
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-05-25 18:43:49 -0400

    feat: updated app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SummonersModule } from './modules/summoners/summoners.module';
+import { MatchesModule } from './modules/matches/matches.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    CacheModule.register({ isGlobal: true }),
+    SummonersModule,
+    MatchesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
